c07bd7c021ad26a66f28ff11b5899a4c77c17f9a
Christian Scharkus <cscharkus@googlemail.com>
2009-08-27 23:55:58 +0200

    init rattlekekz-qt repo with files from rattlekekz qt

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rattlekekz-qt',
+      version='0.1',
+      author="Christian Scharkus",
+      author_email="cscharkus[at]gmail[dot]com",
+      packages=['rattlekekz.qtView'],
+      requires=['PyQt4','rattlekekz'],
+      url="http://kekz.net/",
+      license="GPL v3 or higher"
+     )
