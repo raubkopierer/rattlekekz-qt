9f97e2d21f919a83c802cbb92896c917d96745d9
Christian Scharkus <cscharkus@googlemail.com>
2010-04-23 14:42:38 +0200

    new version for new deb and msi builds

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 import glob
 
 setup(name='rattlekekz-qt',
-      version='20100326',
+      version='20100423',
       author="Christian Scharkus",
       author_email="cscharkus[at]gmail[dot]com",
       packages=['rattlekekz.qtView','twisted.plugins'],
